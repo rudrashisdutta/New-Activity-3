e31e9023e39d265c40b4d2c384420eb297299e81
Knight_Dragon <rudrashis2001@gmail.com>
2020-08-19 13:04:51 +0530

    190820-100p (MainActivity.java and activity_main.xml)

--- app/src/main/java/com/example/example/MainActivity.java
+++ app/src/main/java/com/example/example/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newText=(TextView) findViewById(R.id.textView);
+    }
+    private TextView newText;
+    public void changeText(View View){
+        if(newText.getText().toString().equals("EXAMPLE")) {
+            newText.setText("BUTTON SELECTED!");
+        }
+        else{
+            newText.setText("EXAMPLE");
+        }
     }
 }
